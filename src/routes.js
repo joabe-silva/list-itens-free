e70f89e3b65aaebe831c1a1ed46b5e1052fac293
Joabe Silva <joabesilva533@gmail.com>
2022-11-21 21:46:41 -0300

    modificando layout da listagem de listas

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Login } from './components/Login/Login';
 import { Cadastro } from './components/Cadastro/Cadastro';
-import { MyLists } from './components/MyLists/MyLists';
+import { MyLists } from './components/MyList/MyList';
 
 const RoutesApi = () => (
     <BrowserRouter>
@@ -10,7 +10,7 @@ const RoutesApi = () => (
             <Route path='/' element={ <Login/> } />
             <Route path='*' element={<h1>Not Found</h1>} />
             <Route path='/cadastro' element={ <Cadastro/> } />
-            <Route path='/my-lists' element={ <MyLists/> } />
+            <Route path='/my-list' element={ <MyLists/> } />
         </Routes>
     </BrowserRouter>
 );
