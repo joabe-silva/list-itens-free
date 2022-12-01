346d6d8dfc8e21a0ddbad6aa6858eef2541c2e49
Joabe Silva <joabesilva533@gmail.com>
2022-12-01 00:21:43 -0300

    iniciando validação de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Login } from './components/Login/Login';
@@ -10,6 +9,25 @@ import { ListaCompartilhar } from './components/ListaCompartilhar/ListaCompartil
 import { ListaArquivada } from './components/ListaArquivada/ListaArquivada';
 import { ItemEditar } from './components/ItemEditar/ItemEditar';
 import { Perfil } from './components/Perfil/Perfil';
+
+/*
+function isAuthenticated() {
+    if(sessionStorage.getItem("@AuthFirebase:token")) {
+        return true;
+    }
+    return false;
+}
+
+const CheckRoute = ({ admin, children }) => {
+
+
+    // seu código para autenticação e checar se o usuário pode usar essa rota
+    const user = { admin: false }
+
+    return (!admin || (admin && user.admin)) ? children : <Redirect to="/login"/>
+
+}
+*/
 
 const RoutesApi = () => (
     <BrowserRouter>
